2cecc5d9ad3b81686baa389ced044278cc03ecfb
saurabh <saurabhkeshri074@gmail.com>
2026-05-26 14:59:27 +0530

    Add Backend URL

--- rajbhog-frontend/src/api/axiosInstance.js
+++ rajbhog-frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080", // backend
+  baseURL: "https://rajbhog-backend.onrender.com", // backend
   headers: {
     "Content-Type": "application/json",
   },
